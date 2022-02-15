b12fdc16a2a40ab60b1f08ba00075cea243c7a17
diana_wang <diana_wang@SFT-OIS31522.rp.edu.sg>
2022-02-15 09:57:58 +0800

    demo for CI

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class ResourceCentre {
 
 	public static void main(String[] args) {
-
+// demo for CI
 		ArrayList<Camcorder> camcorderList = new ArrayList<Camcorder>();
 		ArrayList<Chromebook> chromebookList = new ArrayList<Chromebook>();
 
